dfa87dee08c47be62256f62b227eac37a3484298
Ronak Virja <ronak.virja@shunyavkash.com>
2026-07-10 15:46:36 +0530

    created footer section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Header Toggel JS ----------
 const toggleBtn = document.querySelector(".toggle-btn");
 const headerLinkStack = document.querySelector(".header-link-stack");
 const headerDropdownLink = document.querySelector(".header-dropdown-link-wraper");
@@ -11,4 +12,21 @@ toggleBtn.addEventListener("click", () => {
 headerDropdownLink.addEventListener("click", () => {
   headerDropdownLink.classList.toggle("active");
   headerDropdownBlock.classList.toggle("active");
+});
+
+// Close dropdowns if the user clicks anywhere outside of them
+// window.addEventListener("click", () => {
+//   document.querySelectorAll(".dropdown-content").forEach((content) => {
+//     content.classList.remove("show");
+//   });
+// });
+
+// Footer Toggel JS ----------
+const footerTitles = document.querySelectorAll(".footer-link-title");
+footerTitles.forEach((title) => {
+  title.addEventListener("click", () => {
+    const footerBlock = title.nextElementSibling;
+    footerBlock.classList.toggle("active");
+    title.classList.toggle("active");
+  });
 });
